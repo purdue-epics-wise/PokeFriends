11f928c720cf3c94d6ad024397a9b01819bf557f
Shishir Lakkadi <slakkadi@purdue.edu>
2016-07-18 15:58:17 +0530

    Third Commit

--- MapsActivity.java
+++ MapsActivity.java
@@ -1,6 +1,7 @@
 package edu.purdue.shishir.pokefriends;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,28 +18,25 @@ public class MapsActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        btnShowLocation = (Button) findViewById(R.id.Button);
-        // show location button click event
-        btnShowLocation.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View arg0) {
-                // create class object
-                gps = new GPSTracker(MapsActivity.this);
-                // check if GPS enabled
-                if(gps.canGetLocation()){
-                    double latitude = gps.getLatitude();
-                    double longitude = gps.getLongitude();
-                    // \n is for new line
-                    Toast.makeText(getApplicationContext(), "Your Location is - \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
-                }else{
-                    // can't get location
-                    // GPS or Network is not enabled
-                    // Ask user to enable GPS/network in settings
-                    gps.showSettingsAlert();
-                }
-            }
-        });
+        // create class object
+        gps = new GPSTracker(MapsActivity.this);
+        // check if GPS enabled
+        if (gps.canGetLocation()) {
+            double latitude = gps.getLatitude();
+            double longitude = gps.getLongitude();
+
+            Intent i = new Intent(MapsActivity.this, MainActivity.class);
+            Bundle b = new Bundle();
+            b.putDouble("longitude", longitude);
+            b.putDouble("latitude", latitude);
+            i.putExtras(b);
+            startActivity(i);
+        } else {
+            // can't get location
+            // GPS or Network is not enabled
+            // Ask user to enable GPS/network in settings
+            gps.showSettingsAlert();
+        }
     }
 
 }
