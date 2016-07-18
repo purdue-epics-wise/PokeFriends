2a5c8ce5fa4fe2c27b623b1a5fa1dcce695cf290
Shishir Lakkadi <slakkadi@purdue.edu>
2016-07-18 15:21:14 +0530

    First Commit

--- MapsActivity.java
+++ MapsActivity.java
@@ -1,115 +1,44 @@
 package edu.purdue.shishir.pokefriends;
 
-import android.content.Context;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
-import android.support.v4.app.FragmentActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptor;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-
-    private GoogleMap mMap;
-    private SensorManager sensorManager;
-    private Sensor accelerometer;
-    private float last_x, last_y, last_z;
-
-    long lastUpdate = 0;
+import com.google.android.gms.maps.MapFragment;
 
 
+public class MapsActivity extends Activity {
+
+    Button btnShowLocation;
+    // GPSTracker class
+    GPSTracker gps;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-
-        //setup the accelerometer
-        sensorManager = (SensorManager) this.getSystemService(Context.SEARCH_SERVICE);
-        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        sensorManager.registerListener((SensorEventListener) this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-
-
-    }
-
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-    }
-
-    public void onSensorChanged(SensorEvent event) {
-        Sensor mySensor = event.sensor;
-
-        if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            float x = event.values[0];
-            float y = event.values[1];
-            float z = event.values[2];
-
-            long ourTime = System.currentTimeMillis();
-
-            if (Math.abs(ourTime - lastUpdate) > 2000) {
-                SimpleDateFormat date = new SimpleDateFormat("dd-mm-yyyy");
-                String currentDateTime = date.format(new Date());
-
-                lastUpdate = ourTime;
-
-                if (Math.abs(last_x - x) > 10) {
-                    mMap.addMarker(new MarkerOptions().position(new LatLng(37.2324, -80.234234))
-                            .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN)).title("Hey you moved the x-axis" + currentDateTime));
-
+        btnShowLocation = (Button) findViewById(R.id.Button);
+        // show location button click event
+        btnShowLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                // create class object
+                gps = new GPSTracker(MapsActivity.this);
+                // check if GPS enabled
+                if(gps.canGetLocation()){
+                    double latitude = gps.getLatitude();
+                    double longitude = gps.getLongitude();
+                    // \n is for new line
+                    Toast.makeText(getApplicationContext(), "Your Location is - \nLat: " + latitude + "\nLong: " + longitude, Toast.LENGTH_LONG).show();
+                }else{
+                    // can't get location
+                    // GPS or Network is not enabled
+                    // Ask user to enable GPS/network in settings
+                    gps.showSettingsAlert();
                 }
-
-                if (Math.abs(last_y - y) > 10) {
-                    mMap.addMarker(new MarkerOptions().position(new LatLng(37.2324, -80.234234))
-                            .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN)).title("Hey you moved the x-axis" + currentDateTime));
-
-                }
-
-                if (Math.abs(last_z - z) > 10) {
-                    mMap.addMarker(new MarkerOptions().position(new LatLng(37.2324, -80.234234))
-                            .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN)).title("Hey you moved the x-axis" + currentDateTime));
-
-                }
-
-                last_x = x;
-                last_y = y;
-                last_z = z;
             }
-        }
-
+        });
     }
 
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-    }
 }
